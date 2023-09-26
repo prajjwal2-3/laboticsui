eb358eb790d9ecb6b273125955a9ac209ff12b4a
Adarsh <adarsh9540984202@gmail.com>
2023-09-26 14:14:26 +0530

    change in

---
+++ chatbot.js
@@ -0,0 +1,4 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler")
+chatbotToggler.addEventListener("click",() =>
+    document.body.classList.toggle("show-chatbot"))
+sendchatbtn.addEventListener("click", handlechat);
